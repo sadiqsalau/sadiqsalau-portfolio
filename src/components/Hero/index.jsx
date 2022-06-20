adc4490797d37da11b4ad7053e9c026a22214bf1
Sadiq Salau <sadiqsalau888@gmail.com>
2022-06-20 18:34:21 +0100

    Added fredoka one font, changed logo to svg format and updated the hero section

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,27 +1,29 @@
 import { Button, Col, Container, Image, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Logo from "../../assets/images/logo.png";
+import Logo from "../../assets/images/logo.svg";
 import "./index.scss";
 
 const Hero = () => {
   return (
     <div className="hero">
       <Container className="min-vh-100 d-flex flex-column justify-content-center">
-        <Row className="align-items-center flex-row-reverse flex-md-row">
-          <Col className="d-flex justify-content-center mb-3">
+        <Row className="align-items-center justify-content-center">
+          <Col className="d-flex justify-content-center mb-3" md={4}>
             <Image fluid src={Logo} className="hero-logo" />
           </Col>
           <Col md={7}>
-            <h1 className="hero-h1">
-              Hi, I'm <span>Sadiq Salau</span>
-            </h1>
-            <h2 className="hero-h2">A Web Developer</h2>
-            <p className="fst-italic">
-              I develop websites and web applications
-            </p>
-            <Button as={Link} to="#contact">
-              Contact Me
-            </Button>
+            <div className="hero-content">
+              <h1 className="hero-h1">
+                Hi, I'm <span>Sadiq Salau</span>
+              </h1>
+              <h2 className="hero-h2 fst-italic">A Web Developer</h2>
+              <p className="fst-italic">
+                I develop websites and web applications
+              </p>
+              <Button as={Link} to="#contact">
+                Contact Me
+              </Button>
+            </div>
           </Col>
         </Row>
       </Container>
