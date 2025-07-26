828101a58304d0222d8775969141e2391e4e2c6f
Sadiq Salau <sadiqsalau888@gmail.com>
2025-07-26 17:59:50 +0100

    Added vite imagetools plugin types

---
+++ src/types/imagetools.d.ts
@@ -0,0 +1,50 @@
+// Type declarations for vite-imagetools
+declare module "*&format=webp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*&format=avif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*&format=jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*&format=jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*&format=png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png?*" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg?*" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg?*" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp?*" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.avif?*" {
+  const src: string;
+  export default src;
+}
