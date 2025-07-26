fcdbfebf69deae12741c4af41174ab26b9dba237
Sadiq Salau <sadiqsalau888@gmail.com>
2025-07-26 17:34:21 +0100

    feat: Added vite imagetools plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), imagetools()],
 });
