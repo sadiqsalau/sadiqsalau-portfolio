f2c1a182b6ee34b2869b8619cb25505fe65f15a9
Sadiq Salau <sadiqsalau888@gmail.com>
2025-07-26 17:15:31 +0100

    feat: Added TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(), react()],
+});
