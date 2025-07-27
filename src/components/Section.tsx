aa17d9525b3a3fae47eac9f2da38e57b4e47dce8
Sadiq Salau <sadiqsalau888@gmail.com>
2025-07-27 06:28:11 +0100

    feat: Include passed props

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,11 +1,23 @@
-interface SectionProps {
+import React from "react";
+import type { ClassValue } from "clsx";
+
+import { cn } from "../lib/utils";
+
+type SectionProps = React.HTMLAttributes<HTMLElement> & {
   title: string;
   children: React.ReactNode;
-}
+  className: ClassValue;
+};
 
-export default function Section({ title, children }: SectionProps) {
+export default function Section({ title, children, ...props }: SectionProps) {
   return (
-    <section className="mx-auto max-w-7xl py-12 px-4 flex flex-col gap-4">
+    <section
+      {...props}
+      className={cn(
+        "mx-auto max-w-7xl py-12 px-4 flex flex-col gap-4",
+        props.className
+      )}
+    >
       <h1 className="text-4xl font-light text-green-500">{title}</h1>
       {children}
     </section>
