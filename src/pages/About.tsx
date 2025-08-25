bd4f477e75eba024a3d42bd76534a890ef3f4ac4
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-25 17:19:01 +0100

    feat: Added About Page

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import AboutSection from "../components/AboutSection";
+import PageSEO from "../components/PageSEO";
+
+export default function About() {
+  return (
+    <>
+      <PageSEO title="About" description="Learn more about me" />
+      <AboutSection />
+    </>
+  );
+}
