6e584e399a7bd17bf082e3cb435deed5d54764c5
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-25 17:17:05 +0100

    feat: Added Helmet Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 
 import { BrowserRouter } from "react-router";
+import { HelmetProvider } from "react-helmet-async";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
@@ -9,7 +10,9 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </StrictMode>
 );
