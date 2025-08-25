2acdddc9c50bfaa97c0ec092c474a31e4d747ff2
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-25 18:21:26 +0100

    feat: Added SEO details

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
 import Hero from "../components/Hero";
+import PageSEO from "../components/PageSEO";
 
 export default function Home() {
   return (
     <>
+      <PageSEO
+        isFullTitle
+        title="Sadiq Salau — Software Engineer"
+        description="A curated showcase of my best work. I focus on performance, UX, and offline-first readiness."
+        images={["https://sadiqsalau.vercel.app/og.jpg"]}
+      />
       <Hero />
     </>
   );
