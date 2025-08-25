4a45cc594046afaee0372122f56bce305c38cd2c
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-25 17:20:22 +0100

    feat: Display only hero on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,9 @@
-import AboutSection from "../components/AboutSection";
-import ContactsSection from "../components/ContactsSection";
-import FeaturedProjectsSection from "../components/FeaturedProjectsSection";
-import Footer from "../components/Footer";
 import Hero from "../components/Hero";
-import SectionsContainer from "../components/SectionsContainer";
-import TestimonialsSection from "../components/TestimonialsSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <SectionsContainer>
-        <AboutSection />
-        <FeaturedProjectsSection />
-        <TestimonialsSection />
-        <ContactsSection />
-      </SectionsContainer>
-
-      <Footer />
     </>
   );
 }
