88ffedcaa19e1b6d4be36cb317ebab88fa10709d
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-25 17:19:20 +0100

    feat: Added Contact Page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,11 @@
+import ContactsSection from "../components/ContactsSection";
+import PageSEO from "../components/PageSEO";
+
+export default function Contact() {
+  return (
+    <>
+      <PageSEO title="Contact" description="Get in touch with me" />
+      <ContactsSection />
+    </>
+  );
+}
