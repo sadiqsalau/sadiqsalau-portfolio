05e94cf50e6ce4921bda94c50688b81aee099f22
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-25 17:19:56 +0100

    feat: Removed container

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,30 +1,19 @@
-import { Link } from "react-router";
-import { LuArrowLeft } from "react-icons/lu";
-
-import Footer from "../components/Footer";
+import PageSEO from "../components/PageSEO";
 import ProjectsList from "../components/ProjectsList";
 import Section from "../components/Section";
-import SectionsContainer from "../components/SectionsContainer";
 import projects from "../resources/projects";
 
 export default function Projects() {
   return (
     <>
-      <SectionsContainer>
-        <Link to="/" className="flex items-center gap-2 text-green-500">
-          <LuArrowLeft className="size-6" /> Return to Homepage
-        </Link>
-
-        <Section title="Projects">
-          <p className="text-stone-300">
-            This is a list of my projects. You can browse through them to see
-            the work I've done.
-          </p>
-          <ProjectsList projects={projects} />
-        </Section>
-      </SectionsContainer>
-
-      <Footer />
+      <PageSEO title="Projects" description="A list of my projects" />
+      <Section title="Projects">
+        <p className="text-stone-300">
+          This is a list of my projects. You can browse through them to see the
+          work I've done.
+        </p>
+        <ProjectsList projects={projects} />
+      </Section>
     </>
   );
 }
