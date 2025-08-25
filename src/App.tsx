306f6fc3eadb3091afefd8ce18af25d869746a07
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-25 17:12:46 +0100

    feat: Added Helmet Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { Route, Routes } from "react-router";
 
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import InnerPageLayout from "./layouts/InnerPageLayout";
+import ProjectDetails from "./pages/ProjectDetails";
 import Projects from "./pages/Projects";
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
+      <Route element={<InnerPageLayout />}>
+        <Route path="/projects/:id" element={<ProjectDetails />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
     </Routes>
   );
 }
