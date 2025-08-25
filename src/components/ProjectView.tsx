1b866b364aac4fae4c4e450d53604409f4f807dd
Sadiq Salau <sadiqsalau888@gmail.com>
2025-08-25 17:15:43 +0100

    feat: Added Project View Component

---
+++ src/components/ProjectView.tsx
@@ -0,0 +1,80 @@
+import "yet-another-react-lightbox/styles.css";
+
+import Lightbox from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import type { ClickHandler, Photo } from "react-photo-album";
+import { ErrorBoundary } from "react-error-boundary";
+import { useLocation, useNavigate } from "react-router";
+
+import ProjectHeader from "./ProjectHeader";
+import useNavigateBack from "../hooks/useNavigateBack";
+import type { Project } from "../types/project";
+import { ProjectPhotoAlbum } from "./ProjectPhotoAlbum";
+import { cn } from "../lib/utils";
+
+interface ProjectViewProps {
+  project: Project;
+}
+
+export default function ProjectView({ project }: ProjectViewProps) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const navigateBack = useNavigateBack();
+  const currentImageIndex = location.state?.image ?? -1;
+  const showLightbox = currentImageIndex >= 0;
+
+  const openLightbox: ClickHandler<Photo> = ({ index }) => {
+    navigate(location, {
+      replace: false,
+      state: { ...location.state, image: index },
+    });
+  };
+
+  return (
+    <>
+      <ProjectHeader project={project} />
+
+      {/* Overview */}
+      {project.overview ? (
+        <div
+          className={cn(
+            "p-4 rounded-xl",
+            "bg-stone-800 text-stone-200",
+            "whitespace-pre-line font-medium",
+            "flex flex-col gap-2"
+          )}
+        >
+          {typeof project.overview === "function" ? (
+            project.overview()
+          ) : (
+            <p>{project.overview}</p>
+          )}
+        </div>
+      ) : null}
+
+      {/* Photos */}
+      <ErrorBoundary
+        fallback={<div className="text-red-500">Error loading photos</div>}
+      >
+        {project.previewImages?.length ? (
+          <ProjectPhotoAlbum
+            photos={project.previewImages}
+            onClick={openLightbox}
+          />
+        ) : null}
+      </ErrorBoundary>
+
+      {/* Lightbox */}
+      <Lightbox
+        open={showLightbox}
+        index={currentImageIndex}
+        close={() => navigateBack()}
+        slides={project.images}
+        plugins={[Zoom]}
+        animation={{
+          fade: 0,
+        }}
+      />
+    </>
+  );
+}
